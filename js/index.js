12bd9eb9c7f8ffbdcb30ac10cbc0ad2c8f6a406f
Dominique <domizm122788@outlook.com>
2026-05-22 07:32:36 -0600

    Styling added and changed

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const body = document.querySelector("body");
+const footer = document.createElement("footer");
+footer.textContent = "Copyright 2026 - ALL RIGHTS RESERVED";
+
+body.appendChild(footer);
