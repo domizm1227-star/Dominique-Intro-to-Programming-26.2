dc478294d667a052ef66b6b1cc03fea6dd86c9ab
Dominique <domizm122788@outlook.com>
2026-05-28 02:01:35 -0600

    switched name and year in copyright text

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
 const copyrightParagraph = document.createElement('p');
-copyrightParagraph.textContent =`${new Date().getFullYear()} Dominique Medrano`;
+copyrightParagraph.textContent =`Dominique Medrano ${new Date().getFullYear()}`;
 footerElement.appendChild(copyrightParagraph);
 
 const skills = ['HTML', 'CSS', 'JAVASCRIPT', 'Customer-Service', 'AI-Training', 'Problem-Solving', 'Git & GitHub'];
